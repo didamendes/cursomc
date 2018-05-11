dfec21ec82f23b5d79b228c61f192286ed8b4434
Diogo Mendes <didamendes@hotmail.com>
2018-05-11 16:37:38 -0300

    Projeto criado

---
+++ src/main/java/br/com/diogomendes/cursomc/CursmomcApplication.java
@@ -0,0 +1,12 @@
+package br.com.diogomendes.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursmomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursmomcApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/diogomendes/cursomc/CursmomcApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.diogomendes.cursomc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursmomcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
